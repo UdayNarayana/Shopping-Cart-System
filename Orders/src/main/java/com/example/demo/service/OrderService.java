437fcbc70fbf306ca3cbbb61d8b8efd086cbb36e
chetanredy <111568224+chetanredy@users.noreply.github.com>
2022-08-22 22:42:25 +0530

    Add files via upload

---
+++ Orders/src/main/java/com/example/demo/service/OrderService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Repository.OrderRepository;
+import com.example.demo.model.Orders;
+@Service
+public class OrderService {
+	
+	@Autowired
+	OrderRepository orderRepository;
+
+	public List<Orders> getAll() {
+		return orderRepository.findAll();
+	}
+	public Orders placeOrder(Orders order) {
+		return orderRepository.save(order);
+	}
+	public Orders updateOrder(Orders order) {
+		return orderRepository.save(order);
+	}
+	public String deleteOrder() {
+		 orderRepository.deleteAll();
+		 return "orders-deleted";
+	}
+
+
+
+}
