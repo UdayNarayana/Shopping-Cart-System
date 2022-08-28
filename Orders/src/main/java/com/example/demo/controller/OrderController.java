c471ba6d11fcc8352ed848474b178749e3c5e67d
chetanredy <111568224+chetanredy@users.noreply.github.com>
2022-08-28 17:37:21 +0530

    updating

--- Orders/src/main/java/com/example/demo/controller/OrderController.java
+++ Orders/src/main/java/com/example/demo/controller/OrderController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.demo.controller;
 
 import java.util.List;
@@ -12,55 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.Orders;
-import com.example.demo.service.OrderService;
-
-
-@RestController
-@RequestMapping("/orders")
-
-public class OrderController {
-
-	@Autowired
-	OrderService orderService;
-
-	@GetMapping("/getall")
-	public List<Orders> getAll(){
-		return orderService.getAll();
-
-	}
-	@PostMapping("/placeorder")
-	public Orders placeOrder(@RequestBody Orders order){
-		return orderService.placeOrder(order);
-
-	}
-	@PutMapping("/updateorder")
-	public Orders updateOrder(@RequestBody Orders order){
-		return orderService.updateOrder(order);
-
-	}
-	@DeleteMapping("/placeorder")
-	public String deleteOrder(){
-		return orderService.deleteOrder();
-
-	}
-
-
-}
-=======
-package com.example.demo.controller;
-
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.example.demo.model.Address;
 import com.example.demo.model.Orders;
 import com.example.demo.service.OrderService;
 
@@ -73,9 +24,14 @@ public class OrderController {
 	@Autowired
 	OrderService orderService;
 	
-	@GetMapping("/getall")
-	public List<Orders> getAll(){
-		return orderService.getAll();
+	@GetMapping("/getallorders")
+	public List<Orders> getAllOrders(){
+		return orderService.getAllOrders();
+		
+	}
+	@GetMapping("/getalladdress")
+	public List<Address> getAllAddress(){
+		return orderService.getAllAddress();
 		
 	}
 	@PostMapping("/placeorder")
@@ -93,7 +49,21 @@ public class OrderController {
 		return orderService.deleteOrder();
 		
 	}
+	@PostMapping("/addAddress")
+	public Address addAddress(@RequestBody Address address){
+		return orderService.addAddress(address);
+		
+	}
+	@PutMapping("/updateAddress")
+	public Address updateAddress(@RequestBody Address address){
+		return orderService.updateAddress(address);
+		
+	}
+	@DeleteMapping("/deleteAddress")
+	public String deleteAddress(){
+		return orderService.deleteAddress();
+		
+	}
 	 
 
 }
->>>>>>> f1174f2e153822c7d8115f1eb6f6748dd8545ede
--- Orders/src/main/java/com/example/demo/service/OrderService.java
+++ Orders/src/main/java/com/example/demo/service/OrderService.java
@@ -1,52 +1,26 @@
-<<<<<<< HEAD
 package com.example.demo.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.example.demo.Repository.OrderRepository;
-import com.example.demo.model.Orders;
-@Service
-public class OrderService {
-
-	@Autowired
-	OrderRepository orderRepository;
-
-	public List<Orders> getAll() {
-		return orderRepository.findAll();
-	}
-	public Orders placeOrder(Orders order) {
-		return orderRepository.save(order);
-	}
-	public Orders updateOrder(Orders order) {
-		return orderRepository.save(order);
-	}
-	public String deleteOrder() {
-		 orderRepository.deleteAll();
-		 return "orders-deleted";
-	}
 
 
-
-}
-=======
-package com.example.demo.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.Repository.AddressRepository;
 import com.example.demo.Repository.OrderRepository;
+import com.example.demo.model.Address;
 import com.example.demo.model.Orders;
 @Service
 public class OrderService {
 	
 	@Autowired
 	OrderRepository orderRepository;
+	
+	@Autowired
+	AddressRepository addressRepository;
 
-	public List<Orders> getAll() {
+	public List<Orders> getAllOrders() {
 		return orderRepository.findAll();
 	}
 	public Orders placeOrder(Orders order) {
@@ -59,8 +33,20 @@ public class OrderService {
 		 orderRepository.deleteAll();
 		 return "orders-deleted";
 	}
+	public List<Address> getAllAddress() {
+		return addressRepository.findAll();
+	}
+	public Address addAddress(Address address) {
+		return addressRepository.save(address);
+	}
+	public Address updateAddress(Address address) {
+		return addressRepository.save(address);
+	}
+	public String deleteAddress() {
+		addressRepository.deleteAll();
+		 return "address-deleted";
+	}
 
 
 
 }
->>>>>>> f1174f2e153822c7d8115f1eb6f6748dd8545ede
