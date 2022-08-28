095962f7f3b3dbe04536647af32b7edd5367c8de
uday <udaynl619@gmail.com>
2022-08-28 13:52:37 +0530

    added exception handling

---
+++ Profile-microservice/src/main/java/com/eshopping/profile/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.eshopping.profile.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler  {
+
+	@ExceptionHandler(InvalidEmailFormatException.class)
+	public ResponseEntity<ErrorDetails> handleInvalidEmailFormat(InvalidEmailFormatException ex) {
+		ErrorDetails error = new ErrorDetails(
+						ex.getMessage(), 
+						LocalDateTime.now());
+		return new ResponseEntity<>(error,org.springframework.http.HttpStatus.NOT_ACCEPTABLE);
+	}
+
+	@ExceptionHandler(InvalidPasswordFormatException.class)
+	public ResponseEntity<ErrorDetails> handleInvalidPasswordFormat(InvalidPasswordFormatException ex) {
+		ErrorDetails error = new ErrorDetails(
+				ex.getMessage(), 
+				LocalDateTime.now());
+		return new ResponseEntity<>(error,org.springframework.http.HttpStatus.NOT_ACCEPTABLE);
+	}
+	
+	@ExceptionHandler(DuplicateUsernameException.class)
+	public ResponseEntity<ErrorDetails> handleDuplicateUsernameException(DuplicateUsernameException ex) {
+		ErrorDetails error = new ErrorDetails(
+				ex.getMessage(), 
+				LocalDateTime.now());
+		return new ResponseEntity<>(error,org.springframework.http.HttpStatus.NOT_ACCEPTABLE);
+	}
+}
